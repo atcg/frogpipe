cfe80c6c4a3f8f15bc4a43a6c585a39f0de1db04
atcg <evanmelstad@ucla.edu>
2013-02-10 02:10:30 -0800

    Pipeline operational through human/e. coli contaminant removal

--- frogpipe.pl
+++ frogpipe.pl
@@ -37,44 +37,114 @@ if (!defined $adapterFile) {
     die "$usage";
 }
 
-my $logFileName = $sampleID . "_log.txt";
-open(my $logFile, ">>", $logFileName) or die "Couldn't open logfile: $!";
+unless(-d "data") {
+    mkdir "data" or die "can't mkdir data: $!";
+}
+
+unless(-d "data/clean_data") {
+    mkdir "data/clean_data" or die "can't mkdir data/clean_data: $!";
+}
+
+unless(-d "data/sam") {
+    mkdir "data/sam" or die "can't mkdir data/sam: $!";
+}
+
+unless(-d "data/incremental") {
+    mkdir "data/incremental" or die "can't mkdir data/incremental: $!";
+}
+
+print '***** frogpipe.pl, by Evan McCartney-Melstad (evanmelstad@ucla.edu) *****' . "\n";
+print "***** Adapter sequences: $adapterFile *****\n";
+print "***** First fastq file:  $fastqF *****\n";
+print "***** Second fastq file: $fastqR *****\n";
+print "***** Sample identifier: $sampleID *****\n\n\n\n";
+
 
 ## Run Scythe (https://github.com/ucdavis-bioinformatics/scythe)
 ## Usage: scythe -a adapter_file.fasta sequence_file.fastq
+print "***** Running Scythe on each fastq file to clean up 3' ends for adapter contaminants *****\n";
 my $scytheOutputF = $fastqF . "_scythed";
 my $scytheOutputR = $fastqR . "_scythed";
-system("~/bin/scythe/scythe -a $adapterFile -q sanger -o $scytheOutputF $fastqF");
-system("~/bin/scythe/scythe -a $adapterFile -q sanger -o $scytheOutputR $fastqR");
-
+system("~/bin/scythe/scythe -a $adapterFile -q sanger -o data/incremental/$scytheOutputF $fastqF");
+print "\n";
+system("~/bin/scythe/scythe -a $adapterFile -q sanger -o data/incremental/$scytheOutputR $fastqR");
+print "***** Scythe finished running *****\n\n\n";
 
 # Run Sickle (https://github.com/ucdavis-bioinformatics/sickle)
 # Usage: sickle pe -f <paired-end fastq file 1> -r <paired-end fastq file 2> -t <quality type> -o <trimmed pe file 1> -p <trimmed pe file 2> -s <trimmed singles file>
 my $sickleOutputPE_1 = $scytheOutputF . "_sickled";
 my $sickleOutputPE_2 = $scytheOutputR . "_sickled";
 my $sickleOutputSingles = $sampleID . "_scythed_sickled_singletons";
-system("~/bin/sickle/sickle pe -f $scytheOutputF -r $scytheOutputR -q 30 -n -t sanger -o $sickleOutputPE_1 -p $sickleOutputPE_2 -s $sickleOutputSingles");
+print "***** Running Sickle on paired end files to trim to longest reads of acceptable quality *****\n";
+system("~/bin/sickle/sickle pe -f data/incremental/$scytheOutputF -r data/incremental/$scytheOutputR -q 20 -n -t sanger -o data/incremental/$sickleOutputPE_1 -p data/incremental/$sickleOutputPE_2 -s data/incremental/$sickleOutputSingles");
+print "***** Sickle finished running *****\n\n\n";
+
 
 # Merge reads in case the fragment was > 2 times read length (and they overlap):   
-# Usage: flash <mates1.fastq> <mates2.fastq> [options]
-# (Creates histogram of read lengths. For sample 19825, approximately 50% of reads overlapped (indicating fragments less than 500bp)).
-#Something wrong with this setup below--not sure what yet
-system("~/bin/FLASH_v1.0.3/flash $sickleOutputPE_1 $sickleOutputPE_2 -m 10 -M 150 -x .1 -p 33 -o flashed -r 150 -f 750 -s 100");
+my $fastq_join_Output_Prefix = $sampleID . "_scythed_sickled_fastq-joined";
+my $fastq_join_Output1 = $fastq_join_Output_Prefix . ".un1.fastq"; #this file will be created below by fastq-join
+my $fastq_join_Output2 = $fastq_join_Output_Prefix . ".un2.fastq"; #this file will be created below by fastq-join
+my $fastq_join_Output_JOINED = $fastq_join_Output_Prefix . ".join.fastq"; #this file will be created below by fastq-join
+print "***** Running fastq-join on paired end files to merge overlapping paired-end reads (in case some fragments were shorter than 2x read length) *****\n";
+system("~/bin/ea-utils.1.1.2-537/fastq-join data/incremental/$sickleOutputPE_1 data//incremental/$sickleOutputPE_2 -o data/incremental/$fastq_join_Output_Prefix.%.fastq -m 15 -p 1");
+print "***** Fastq-join finished running *****\n\n\n";
+
+#So at this point, we have several useful QC'ed files (filenames below assume usage example from line 9 above).
+# 1. 19825_scythed_sickled_singletons, which is the file that contains the non-paired end reads (when sickle kicked out a poor quality read, its good-quality partner went into this file)
+# 2. 19825_scythed_sickled_fastq-joined.join.fastq, which contains the paired end reads that have been merged by fastq-join
+# 3. 19825_scythed_sickled_fastq-joined.un1.fastq, which contains the QC'ed reads from the first file that have not been merged
+# 4. 19825_scythed_sickled_fastq-joined.un2.fastq, which contains the QC'ed reads from the second file that have not been merged
+
+#Now filter out reads from all of the four files above that align with human or e. coli using bowtie2
+#should I add/remove the --no-mixed or --no-discordant flags?
+my $noHumanReadsSingles = $sickleOutputSingles . "_noHuman";
+my $humanReadsSingles = $sickleOutputSingles . "_HUMAN";
+my $singlesNoHumanSam = $sickleOutputSingles . "_humanfilter_bowtie2.sam";
+
+my $noHumanReadsJoined = $fastq_join_Output_JOINED . "_noHuman";
+my $humanReadsJoined = $fastq_join_Output_JOINED . "_HUMAN";
+my $joinedNoHumanSam = $fastq_join_Output_JOINED . "_humanfilter_bowtie2.sam";
+
+my $noHumanReadsPairs = $sampleID . "pairs_noHuman";
+my $humanReadsPairs = $sampleID . "pairs_HUMAN";
+my $pairsNoHumanSam = $sampleID . "_humanfilter_bowtie2.sam";
+my $noHumanReadsPairsOut1 = $noHumanReadsPairs . ".1";
+my $noHumanReadsPairsOut2 = $noHumanReadsPairs . ".2";
+
+my $noHumanNoEColiSingles = $noHumanReadsSingles . "_noEColi";
+my $noHumanYesEColiSingles = $noHumanReadsSingles . "_YES-EColi";
+my $singlesNoHumanNoEColiSam = $noHumanReadsSingles . "_ecoliFilter_bowtie2.sam";
+
+my $noHumanNoEColiJoined = $noHumanReadsJoined . "_noEColi";
+my $noHumanYesEColiJoined = $noHumanReadsJoined . "_YES-EColi";
+my $joinedNoHumanNoEColiSam = $noHumanReadsJoined . "_ecoliFilter_bowtie2.sam";
+
+my $noHumanNoEColiPairs = $noHumanReadsPairs . "_noEColi";
+my $noHumanYesEColiReadsPairs = $noHumanReadsPairs . "_YES-EColi";
+my $pairsNoHumanNoEColiSam = $noHumanReadsPairs . "_ecoliFilter_bowtie2.sam";
+my $noHumanNoEColiOut1 = $noHumanNoEColiPairs . ".1";
+my $noHumanNoEColiOut2 = $noHumanNoEColiPairs . ".2";
+
+
+my $genomeDir = $ENV{'BOWTIE2_INDEXES'};
+print "***** Checking to see if reads align to the human genome to remove contaminants ****\n";
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/incremental/$noHumanReadsSingles --al data/incremental/$humanReadsSingles -x $genomeDir/hg19 -U data/incremental/$sickleOutputSingles -S data/sam/$singlesNoHumanSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/incremental/$noHumanReadsJoined --al data/incremental/$humanReadsJoined -x $genomeDir/hg19 -U data/incremental/$fastq_join_Output_JOINED -S data/sam/$joinedNoHumanSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --minins 0 --maxins 2000 --no-mixed --no-discordant --threads 7 --un-conc data/incremental/$noHumanReadsPairs --al-conc data/incremental/$humanReadsPairs -x $genomeDir/hg19 -1 data/incremental/$fastq_join_Output1 -2 data/incremental/$fastq_join_Output2 -S data/sam/$pairsNoHumanSam");
+print "***** Finished checking for human contaminants *****\n\n\n";
+
+print "***** Checking to see if reads align to the E. coli genome to remove contaminants *****\n";
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/clean_data/$noHumanNoEColiSingles --al data/incremental/$noHumanYesEColiSingles -x $genomeDir/hg19 -U data/incremental/$noHumanReadsSingles -S data/sam/$singlesNoHumanNoEColiSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/clean_data/$noHumanNoEColiJoined --al data/incremental/$noHumanYesEColiJoined -x $genomeDir/hg19 -U data/incremental/$noHumanReadsJoined -S data/sam/$joinedNoHumanNoEColiSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --minins 0 --maxins 2000 --no-mixed --no-discordant --threads 7 --un-conc data/clean_data/$noHumanNoEColiPairs --al-conc data/incremental/$noHumanYesEColiReadsPairs -x $genomeDir/hg19 -1 data/incremental/$noHumanReadsPairsOut1 -2 data/incremental/$noHumanReadsPairsOut2 -S data/sam/$pairsNoHumanNoEColiSam");
+print "***** Finished checking for E. coli contaminants *****\n\n\n";
 
 
 
-
-
-
-
-
-
-
-#    Map reads to human and e. coli genomes to filter out contamination (bowtie 2)
+#    Map reads to human and e. coli genomes to filter out contamination (bowtie2)
 #        Set a few environmental variables for bowtie2 (this isn't essential, but saves some typing later on):
 #            $BOWTIE2_INDEXES = directory where your genome indices live
 #            $BT2_HOME = directory where the bowtie2 binaries live (something like ~/bin/bowtie2-2.0.6
 #        Install bowtie2, and build genome indices for human/e. coli/anything else that should be filtered out (this takes a long time)
 #        bowtie2 -q Ðphred33 Ðmaxins 2000 -p 7 Ðun ~/Desktop/Frogs_nextgen/Feb8/19825_nohuman_unpaired.txt Ðal ~/Desktop/Frogs_nextgen/Feb8/19825_HUMAN_unpaired.txt Ðun-conc ~/Desktop/Frogs_nextgen/Feb8/19825_noHuman_paired.txt Ðal-conc ~/Desktop/Frogs_nextgen/Feb8/19825_HUMAN_paired.txt -x /mnt/Data1/human_genome/hg19 {-1 ~/Desktop/Frogs_nextgen/Feb8/19825_notCombined_1.fastq -2 ~/Desktop/Frogs_nextgen/Feb8/19825_notCombined_2.fastq | -U ~/Desktop/Frogs_nextgen/Feb8/19825_Combined.fastq}
 #            The above command uses 7 threads (so make sure you have 8 cores, or else set this to 3 or 4), a maximum fragment length of 2000 (which is a lot higher than their default value),
-
