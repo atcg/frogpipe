a2c32297cbc32102843f268ac37222fec819ebd0
atcg <evanmelstad@ucla.edu>
2013-02-26 01:08:18 -0800

    Added ABYSS looping

--- frogpipe.pl
+++ frogpipe.pl
@@ -37,20 +37,29 @@ if (!defined $adapterFile) {
     die "$usage";
 }
 
-unless(-d "data") {
-    mkdir "data" or die "can't mkdir data: $!";
+my $pipeDir = "$sampleID" . "_piped";
+
+unless(-d "$pipeDir") {
+    mkdir "$pipeDir" or die "can't mkdir $pipeDir: $!";
 }
 
-unless(-d "data/clean_data") {
-    mkdir "data/clean_data" or die "can't mkdir data/clean_data: $!";
+unless(-d "$pipeDir/data") {
+    mkdir "$pipeDir/data" or die "can't mkdir $pipeDir/data: $!";
 }
 
-unless(-d "data/sam") {
-    mkdir "data/sam" or die "can't mkdir data/sam: $!";
+unless(-d "$pipeDir/data/clean_data") {
+    mkdir "$pipeDir/data/clean_data" or die "can't mkdir $pipeDir/data/clean_data: $!";
 }
 
-unless(-d "data/incremental") {
-    mkdir "data/incremental" or die "can't mkdir data/incremental: $!";
+unless(-d "$pipeDir/data/sam") {
+    mkdir "$pipeDir/data/sam" or die "can't mkdir $pipeDir/data/sam: $!";
+}
+
+unless(-d "$pipeDir/data/incremental") {
+    mkdir "$pipeDir/data/incremental" or die "can't mkdir $pipeDir/data/incremental: $!";
+}
+unless(-d "$pipeDir/data/ABYSS") {
+    mkdir "$pipeDir/data/ABYSS" or die "can't mkdir $pipeDir/data/ABYSS: $!";
 }
 
 print '***** frogpipe.pl, by Evan McCartney-Melstad (evanmelstad@ucla.edu) *****' . "\n";
@@ -71,9 +80,9 @@ print "***** Sample identifier: $sampleID *****\n\n\n\n";
 print "***** Running Scythe on each fastq file to clean up 3' ends for adapter contaminants *****\n";
 my $scytheOutputF = $fastqF . "_scythed";
 my $scytheOutputR = $fastqR . "_scythed";
-system("~/bin/scythe/scythe -a $adapterFile -q sanger -o data/incremental/$scytheOutputF $fastqF");
+system("~/bin/scythe/scythe -a $adapterFile -q sanger -o $pipeDir/data/incremental/$scytheOutputF $fastqF");
 print "\n";
-system("~/bin/scythe/scythe -a $adapterFile -q sanger -o data/incremental/$scytheOutputR $fastqR");
+system("~/bin/scythe/scythe -a $adapterFile -q sanger -o $pipeDir/data/incremental/$scytheOutputR $fastqR");
 print "***** Scythe finished running *****\n\n\n";
 
 # Run Sickle (https://github.com/ucdavis-bioinformatics/sickle)
@@ -82,7 +91,7 @@ my $sickleOutputPE_1 = $scytheOutputF . "_sickled";
 my $sickleOutputPE_2 = $scytheOutputR . "_sickled";
 my $sickleOutputSingles = $sampleID . "_scythed_sickled_singletons";
 print "***** Running Sickle on paired end files to trim to longest reads of acceptable quality *****\n";
-system("~/bin/sickle/sickle pe -f data/incremental/$scytheOutputF -r data/incremental/$scytheOutputR -q 20 -n -t sanger -o data/incremental/$sickleOutputPE_1 -p data/incremental/$sickleOutputPE_2 -s data/incremental/$sickleOutputSingles");
+system("~/bin/sickle/sickle pe -f $pipeDir/data/incremental/$scytheOutputF -r $pipeDir/data/incremental/$scytheOutputR -q 20 -n -t sanger -o $pipeDir/data/incremental/$sickleOutputPE_1 -p $pipeDir/data/incremental/$sickleOutputPE_2 -s $pipeDir/data/incremental/$sickleOutputSingles");
 print "***** Sickle finished running *****\n\n\n";
 
 
@@ -92,7 +101,7 @@ my $fastq_join_Output1 = $fastq_join_Output_Prefix . ".un1.fastq"; #this file wi
 my $fastq_join_Output2 = $fastq_join_Output_Prefix . ".un2.fastq"; #this file will be created below by fastq-join
 my $fastq_join_Output_JOINED = $fastq_join_Output_Prefix . ".join.fastq"; #this file will be created below by fastq-join
 print "***** Running fastq-join on paired end files to merge overlapping paired-end reads (in case some fragments were shorter than 2x read length) *****\n";
-system("~/bin/ea-utils/fastq-join data/incremental/$sickleOutputPE_1 data/incremental/$sickleOutputPE_2 -o data/incremental/$fastq_join_Output_Prefix.%.fastq -m 15 -p 1");
+system("~/bin/ea-utils/fastq-join $pipeDir/data/incremental/$sickleOutputPE_1 $pipeDir/data/incremental/$sickleOutputPE_2 -o $pipeDir/data/incremental/$fastq_join_Output_Prefix.%.fastq -m 15 -p 1");
 print "***** Fastq-join finished running *****\n\n\n";
 
 #So at this point, we have several useful QC'ed files (filenames below assume usage example from line 9 above).
@@ -134,17 +143,59 @@ my $noHumanNoEColiOut2 = $noHumanNoEColiPairs . ".2";
 
 my $genomeDir = $ENV{'BOWTIE2_INDEXES'};
 print "***** Checking to see if reads align to the human genome to remove contaminants ****\n";
-system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/incremental/$noHumanReadsSingles --al data/incremental/$humanReadsSingles -x $genomeDir/hg19 -U data/incremental/$sickleOutputSingles -S data/sam/$singlesNoHumanSam");
-system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/incremental/$noHumanReadsJoined --al data/incremental/$humanReadsJoined -x $genomeDir/hg19 -U data/incremental/$fastq_join_Output_JOINED -S data/sam/$joinedNoHumanSam");
-system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --minins 0 --maxins 2000 --no-mixed --no-discordant --threads 7 --un-conc data/incremental/$noHumanReadsPairs --al-conc data/incremental/$humanReadsPairs -x $genomeDir/hg19 -1 data/incremental/$fastq_join_Output1 -2 data/incremental/$fastq_join_Output2 -S data/sam/$pairsNoHumanSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un $pipeDir/data/incremental/$noHumanReadsSingles --al $pipeDir/data/incremental/$humanReadsSingles -x $genomeDir/hg19 -U $pipeDir/data/incremental/$sickleOutputSingles -S $pipeDir/data/sam/$singlesNoHumanSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un $pipeDir/data/incremental/$noHumanReadsJoined --al $pipeDir/data/incremental/$humanReadsJoined -x $genomeDir/hg19 -U $pipeDir/data/incremental/$fastq_join_Output_JOINED -S $pipeDir/data/sam/$joinedNoHumanSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --minins 0 --maxins 2000 --no-mixed --no-discordant --threads 7 --un-conc $pipeDir/data/incremental/$noHumanReadsPairs --al-conc $pipeDir/data/incremental/$humanReadsPairs -x $genomeDir/hg19 -1 $pipeDir/data/incremental/$fastq_join_Output1 -2 $pipeDir/data/incremental/$fastq_join_Output2 -S $pipeDir/data/sam/$pairsNoHumanSam");
 print "***** Finished checking for human contaminants *****\n\n\n";
 
 print "***** Checking to see if reads align to the E. coli genome to remove contaminants *****\n";
-system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/clean_data/$noHumanNoEColiSingles --al data/incremental/$noHumanYesEColiSingles -x $genomeDir/ecoliK12 -U data/incremental/$noHumanReadsSingles -S data/sam/$singlesNoHumanNoEColiSam");
-system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un data/clean_data/$noHumanNoEColiJoined --al data/incremental/$noHumanYesEColiJoined -x $genomeDir/ecoliK12 -U data/incremental/$noHumanReadsJoined -S data/sam/$joinedNoHumanNoEColiSam");
-system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --minins 0 --maxins 2000 --no-mixed --no-discordant --threads 7 --un-conc data/clean_data/$noHumanNoEColiPairs --al-conc data/incremental/$noHumanYesEColiReadsPairs -x $genomeDir/ecoliK12 -1 data/incremental/$noHumanReadsPairsOut1 -2 data/incremental/$noHumanReadsPairsOut2 -S data/sam/$pairsNoHumanNoEColiSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un $pipeDir/data/clean_data/$noHumanNoEColiSingles --al $pipeDir/data/incremental/$noHumanYesEColiSingles -x $genomeDir/ecoli -U $pipeDir/data/incremental/$noHumanReadsSingles -S $pipeDir/data/sam/$singlesNoHumanNoEColiSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --threads 7 --un $pipeDir/data/clean_data/$noHumanNoEColiJoined --al $pipeDir/data/incremental/$noHumanYesEColiJoined -x $genomeDir/ecoli -U $pipeDir/data/incremental/$noHumanReadsJoined -S $pipeDir/data/sam/$joinedNoHumanNoEColiSam");
+system("~/bin/bowtie2-2.0.6/bowtie2 -q --phred33 --minins 0 --maxins 2000 --no-mixed --no-discordant --threads 7 --un-conc $pipeDir/data/clean_data/$noHumanNoEColiPairs --al-conc $pipeDir/data/incremental/$noHumanYesEColiReadsPairs -x $genomeDir/ecoli -1 $pipeDir/data/incremental/$noHumanReadsPairsOut1 -2 $pipeDir/data/incremental/$noHumanReadsPairsOut2 -S $pipeDir/data/sam/$pairsNoHumanNoEColiSam");
 print "***** Finished checking for E. coli contaminants *****\n\n\n";
 
 
-#de novo assembly of reads into contigs
-#Abyss
+#de novo assembly of reads into contigs using Abyss
+#This section is based on a script by Mark Phuong
+print "***** Running de novo assembly of reads using multiple kmer and c and e values in Abyss *****.\n";
+
+my @abysskmer = qw(21 31 41 51 61);
+my @cevalue = qw(10 20);
+
+#merge joined and singleton reads into a single file
+my $joinedQCed = $sampleID . "scythed_sickled_nohuman_noecoli_combined_joined_and_singletons.fastq";
+system("cat $pipeDir/data/clean_data/$noHumanNoEColiSingles $pipeDir/data/clean_data/$noHumanNoEColiJoined > $pipeDir/data/clean_data/$joinedQCed");
+
+
+
+###################
+foreach my $kmer (@abysskmer) {
+    foreach my $ce (@cevalue) {
+        print "***** Running Abyss for $sampleID reads at kmer = $kmer and c and e both = $ce *****\n\n";
+        my $outfile = $sampleID . "_kmer" . $kmer . "_ce" . $ce;
+        print "Command = abyss-pe name=$pipeDir/data/ABYSS/$outfile k=$kmer n=10 c=$ce e=$ce in='$pipeDir/data/clean_data/$noHumanNoEColiOut1 $pipeDir/data/clean_data/$noHumanNoEColiOut2' se='$pipeDir/data/clean_data/$joinedQCed'\n\n";
+        system("abyss-pe np=4 name=$pipeDir/data/ABYSS/$outfile k=$kmer n=10 c=$ce e=$ce in='$pipeDir/data/clean_data/$noHumanNoEColiOut1 $pipeDir/data/clean_data/$noHumanNoEColiOut2' se='$pipeDir/data/clean_data/$joinedQCed'");
+        print "***** Finished running Abyss for $sampleID reads at kmer = $kmer and c and e both = $ce *****\n\n\n"
+    }
+    
+    
+    
+}
+
+#Code below by Mark shows how he cleans up extraneous files
+####foreach my $kmer (@abysskmer) {
+####	foreach my $cevalue (@cevalue) {
+####		my $out = $name . "_kmer" . $kmer . "_ce" . $cevalue;
+####		system ("abyss-pe mpirun='/home/analysis/bin/mpirun --hostfile /home/analysis/Desktop/hostfile' np=12 k=$kmer n=10 E=0 c=$cevalue e=$cevalue in='$read1 $read2' se='$unpaired' name=$dir/$out");
+####		system("rm $dir/*.adj");
+####		system("rm $dir/*path*");
+####		system("rm $dir/*.dot");
+####		#system("mv $dir/*contigs* $dir/contig");
+####		#system("mv $dir/*stats $dir/contig");
+####		#system("mv $dir/*-6.fa $dir/contig");
+####		system("rm $dir/*.hist");
+####		#system("rm $dir/*.fa");
+####		system("rm $dir/*.dist");	
+####	}
+####}
+
